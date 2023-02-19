be83fb0926050161698853d1238c4715c61376e2
José Pedro Valentim Oliveira <zeca1997@hotmail.com>
2023-02-19 15:33:22 +0100

    change(scraper): Changes scraper to be class based

--- helpers.js
+++
@@ -1,7 +0,0 @@
-import all_pairings from './pairings.json' assert { type: "json" }
-
-export const find_pairing = (name,round) => {
-    return all_pairings.pairings[`round_${round}`].filter((pairing) => {
-        return pairing.player_1.name === name || pairing.player_2.name === name
-    })
-}
---
+++ src/classes/Pairing.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+import { Pairing } from "../types/types";
+
+export class PairingClass {
+  public constructor(private name: string, private file_path: string) {}
+
+  public async get() {
+    const pairings = require(this.file_path);
+
+    return pairings.filter((pairing: Pairing) => {
+      return (
+        pairing.player_1.name === this.name ||
+        pairing.player_2.name === this.name
+      );
+    });
+  }
+}
